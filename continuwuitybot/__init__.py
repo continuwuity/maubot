d7eb115154e66061600a4e6f31ff52669e876cd0
nexy7574 <git@nexy7574.co.uk>
2025-09-08 00:13:07 +0100

    Fix issue/PR getter

--- continuwuitybot/__init__.py
+++ continuwuitybot/__init__.py
@@ -1,5 +1,4 @@
 import asyncio
-import re
 from typing import Type
 
 import aiohttp
@@ -17,12 +16,13 @@ class Config(BaseProxyConfig):
         helper.copy("forge")
 
 class ContinuwuityHelper(Plugin):
-    RE_ISSUE_N = re.compile(r"((\w+)?/?(\w+))?#(\d+)")
     cs: aiohttp.ClientSession | None = None
 
     async def start(self) -> None:
         self.config.load_and_update()
-        self.cs = aiohttp.ClientSession(base_url=self.forge + "/api/v1")
+        self.cs = aiohttp.ClientSession(
+            base_url=self.forge
+        )
 
     async def stop(self) -> None:
         pass
@@ -43,9 +43,13 @@ class ContinuwuityHelper(Plugin):
     def forge(self) -> str:
         return self.config.get("forge", "https://forgejo.ellis.link")
 
+    @property
+    def base_url(self):
+        return self.forge.rstrip("/") + "/api/v1"
+
     async def get_issue(self, n: int, repo: str = "continuwuation/continuwuity") -> dict | None | Exception:
         try:
-            async with self.cs.get("/repos/%s/issues/%d" % (repo, n)) as resp:
+            async with self.http.get(self.base_url + "/repos/%s/issues/%d" % (repo, n)) as resp:
                 resp.raise_for_status()
                 return await resp.json()
         except aiohttp.ClientResponseError as e:
@@ -53,25 +57,38 @@ class ContinuwuityHelper(Plugin):
                 return None
             return e
 
-    @command.passive(RE_ISSUE_N, multiple=True)
+    async def get_pull_request(self, n: int, repo: str = "continuwuation/continuwuity") -> dict | None | Exception:
+        """
+        Get a pull request by number.
+
+        :param n: The pull request number
+        :param repo: The repository to pull from. Defaults to "continuwuation/continuwuity"
+        :return: The data, None if not found
+        """
+        try:
+            async with self.http.get(self.base_url + "/repos/%s/pulls/%d" % (repo, n)) as resp:
+                resp.raise_for_status()
+                return await resp.json()
+        except aiohttp.ClientResponseError as e:
+            if e.status == 404:
+                return None
+            return e
+
+    @command.passive(r"([a-zA-Z]+/)?([a-zA-Z]+)?[#!](\d+)", multiple=True)
     async def on_issue_number(self, evt: MessageEvent, matches: list[tuple[str]]):
+        await self.client.set_typing(evt.room_id, 30_000)
         t: list[asyncio.Task] = []
         async with asyncio.TaskGroup() as tg:
             for match_set in matches:
                 m = list(match_set)
                 full = m.pop(0)
-                org = self.main_org
-                # don't actually use any of the regex groups for groups. regex sucks.
-                p, n = full.rsplit("#", 1)
-                try:
-                    n = int(n)
-                except ValueError:
-                    # invalid
-                    continue
-                if "/" in p:
-                    org, repo = p.split("/", 1)
-                else:
-                    repo = p
+                org = m.pop(0) or self.main_org
+                # Trim trailing slash if present
+                if org.endswith("/"):
+                    org = org[:-1]
+                repo = m.pop(0) or self.main_repo
+                n = int(m.pop(0))
+                self.log.info("Fetching issue %d from %s/%s", n, org, repo)
                 t.append(
                     tg.create_task(self.get_issue(n, f"{org}/{repo}"), name=full)
                 )
@@ -85,7 +102,7 @@ class ContinuwuityHelper(Plugin):
                 self.log.error("Error while fetching %s: %s", task.get_name(), result, exc_info=result)
                 continue
             lines.append(
-                "* [#{0.number} ({0.state}): {0.title}]({0.html_url}) by [{0.user.username}]({0.user.html_url})".format(
+                "* [#{0[number]} ({0[state]}): {0[title]}]({0[html_url]}) by [{0[user][username]}]({0[user][html_url]})".format(
                     result
                 )
             )
